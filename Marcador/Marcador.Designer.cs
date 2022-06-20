e5fae9688cabf9a585361d6f8e2e57996d7468dc
Fernando <felupy@gmail.com>
2022-06-20 21:24:19 +0200

    Change live board size

--- Marcador/Marcador.Designer.cs
+++ Marcador/Marcador.Designer.cs
@@ -34,15 +34,9 @@ namespace Marcador
             this.Team2Label = new System.Windows.Forms.Label();
             this.ScoreLabel = new System.Windows.Forms.Label();
             this.Team1Label = new System.Windows.Forms.Label();
-            this.Team2Split = new System.Windows.Forms.Splitter();
-            this.ScoreSplit = new System.Windows.Forms.Splitter();
-            this.Team1Split = new System.Windows.Forms.Splitter();
             this.TimeOutLabel = new System.Windows.Forms.Label();
             this.CronoLabel = new System.Windows.Forms.Label();
             this.HalfLabel = new System.Windows.Forms.Label();
-            this.splitter3 = new System.Windows.Forms.Splitter();
-            this.splitter2 = new System.Windows.Forms.Splitter();
-            this.splitter1 = new System.Windows.Forms.Splitter();
             this.ExpulsionDataView = new System.Windows.Forms.DataGridView();
             this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -60,9 +54,9 @@ namespace Marcador
             // ScoreBoardPanel
             // 
             this.ScoreBoardPanel.Controls.Add(this.ScoreBoardSplitContainer);
-            this.ScoreBoardPanel.Location = new System.Drawing.Point(38, 27);
+            this.ScoreBoardPanel.Location = new System.Drawing.Point(18, 15);
             this.ScoreBoardPanel.Name = "ScoreBoardPanel";
-            this.ScoreBoardPanel.Size = new System.Drawing.Size(279, 78);
+            this.ScoreBoardPanel.Size = new System.Drawing.Size(295, 108);
             this.ScoreBoardPanel.TabIndex = 0;
             // 
             // ScoreBoardSplitContainer
@@ -77,29 +71,24 @@ namespace Marcador
             this.ScoreBoardSplitContainer.Panel1.Controls.Add(this.Team2Label);
             this.ScoreBoardSplitContainer.Panel1.Controls.Add(this.ScoreLabel);
             this.ScoreBoardSplitContainer.Panel1.Controls.Add(this.Team1Label);
-            this.ScoreBoardSplitContainer.Panel1.Controls.Add(this.Team2Split);
-            this.ScoreBoardSplitContainer.Panel1.Controls.Add(this.ScoreSplit);
-            this.ScoreBoardSplitContainer.Panel1.Controls.Add(this.Team1Split);
             // 
             // ScoreBoardSplitContainer.Panel2
             // 
             this.ScoreBoardSplitContainer.Panel2.Controls.Add(this.TimeOutLabel);
             this.ScoreBoardSplitContainer.Panel2.Controls.Add(this.CronoLabel);
             this.ScoreBoardSplitContainer.Panel2.Controls.Add(this.HalfLabel);
-            this.ScoreBoardSplitContainer.Panel2.Controls.Add(this.splitter3);
-            this.ScoreBoardSplitContainer.Panel2.Controls.Add(this.splitter2);
-            this.ScoreBoardSplitContainer.Panel2.Controls.Add(this.splitter1);
-            this.ScoreBoardSplitContainer.Size = new System.Drawing.Size(279, 78);
-            this.ScoreBoardSplitContainer.SplitterDistance = 46;
+            this.ScoreBoardSplitContainer.Size = new System.Drawing.Size(295, 108);
+            this.ScoreBoardSplitContainer.SplitterDistance = 62;
             this.ScoreBoardSplitContainer.TabIndex = 0;
             // 
             // Team2Label
             // 
             this.Team2Label.BackColor = System.Drawing.Color.RoyalBlue;
-            this.Team2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Team2Label.Location = new System.Drawing.Point(192, 9);
+            this.Team2Label.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Team2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Team2Label.Location = new System.Drawing.Point(194, 0);
             this.Team2Label.Name = "Team2Label";
-            this.Team2Label.Size = new System.Drawing.Size(84, 29);
+            this.Team2Label.Size = new System.Drawing.Size(101, 62);
             this.Team2Label.TabIndex = 5;
             this.Team2Label.Text = "TEAM2";
             this.Team2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -107,11 +96,12 @@ namespace Marcador
             // ScoreLabel
             // 
             this.ScoreLabel.BackColor = System.Drawing.Color.Black;
+            this.ScoreLabel.Dock = System.Windows.Forms.DockStyle.Left;
             this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ScoreLabel.ForeColor = System.Drawing.Color.White;
-            this.ScoreLabel.Location = new System.Drawing.Point(102, 9);
+            this.ScoreLabel.Location = new System.Drawing.Point(104, 0);
             this.ScoreLabel.Name = "ScoreLabel";
-            this.ScoreLabel.Size = new System.Drawing.Size(78, 29);
+            this.ScoreLabel.Size = new System.Drawing.Size(90, 62);
             this.ScoreLabel.TabIndex = 4;
             this.ScoreLabel.Text = "0 - 0";
             this.ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -119,48 +109,24 @@ namespace Marcador
             // Team1Label
             // 
             this.Team1Label.BackColor = System.Drawing.Color.White;
-            this.Team1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Team1Label.Location = new System.Drawing.Point(3, 9);
+            this.Team1Label.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Team1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Team1Label.Location = new System.Drawing.Point(0, 0);
             this.Team1Label.Name = "Team1Label";
-            this.Team1Label.Size = new System.Drawing.Size(87, 29);
+            this.Team1Label.Size = new System.Drawing.Size(104, 62);
             this.Team1Label.TabIndex = 3;
             this.Team1Label.Text = "TEAM1";
             this.Team1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Team2Split
-            // 
-            this.Team2Split.BackColor = System.Drawing.Color.RoyalBlue;
-            this.Team2Split.Location = new System.Drawing.Point(186, 0);
-            this.Team2Split.Name = "Team2Split";
-            this.Team2Split.Size = new System.Drawing.Size(98, 46);
-            this.Team2Split.TabIndex = 2;
-            this.Team2Split.TabStop = false;
-            // 
-            // ScoreSplit
-            // 
-            this.ScoreSplit.BackColor = System.Drawing.Color.Black;
-            this.ScoreSplit.Location = new System.Drawing.Point(96, 0);
-            this.ScoreSplit.Name = "ScoreSplit";
-            this.ScoreSplit.Size = new System.Drawing.Size(90, 46);
-            this.ScoreSplit.TabIndex = 1;
-            this.ScoreSplit.TabStop = false;
-            // 
-            // Team1Split
-            // 
-            this.Team1Split.BackColor = System.Drawing.Color.White;
-            this.Team1Split.Location = new System.Drawing.Point(0, 0);
-            this.Team1Split.Name = "Team1Split";
-            this.Team1Split.Size = new System.Drawing.Size(96, 46);
-            this.Team1Split.TabIndex = 0;
-            this.Team1Split.TabStop = false;
-            // 
             // TimeOutLabel
             // 
             this.TimeOutLabel.BackColor = System.Drawing.Color.LightGray;
-            this.TimeOutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TimeOutLabel.Location = new System.Drawing.Point(193, 4);
+            this.TimeOutLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.TimeOutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeOutLabel.Location = new System.Drawing.Point(194, 0);
             this.TimeOutLabel.Name = "TimeOutLabel";
-            this.TimeOutLabel.Size = new System.Drawing.Size(83, 22);
+            this.TimeOutLabel.Padding = new System.Windows.Forms.Padding(3);
+            this.TimeOutLabel.Size = new System.Drawing.Size(104, 42);
             this.TimeOutLabel.TabIndex = 5;
             this.TimeOutLabel.Text = "0:00";
             this.TimeOutLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -169,11 +135,13 @@ namespace Marcador
             // CronoLabel
             // 
             this.CronoLabel.BackColor = System.Drawing.Color.Black;
-            this.CronoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CronoLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.CronoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CronoLabel.ForeColor = System.Drawing.Color.White;
-            this.CronoLabel.Location = new System.Drawing.Point(102, 5);
+            this.CronoLabel.Location = new System.Drawing.Point(104, 0);
+            this.CronoLabel.Margin = new System.Windows.Forms.Padding(0);
             this.CronoLabel.Name = "CronoLabel";
-            this.CronoLabel.Size = new System.Drawing.Size(78, 21);
+            this.CronoLabel.Size = new System.Drawing.Size(90, 42);
             this.CronoLabel.TabIndex = 4;
             this.CronoLabel.Text = "00:00";
             this.CronoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -181,37 +149,15 @@ namespace Marcador
             // HalfLabel
             // 
             this.HalfLabel.BackColor = System.Drawing.Color.LightGray;
-            this.HalfLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HalfLabel.Location = new System.Drawing.Point(7, 4);
+            this.HalfLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.HalfLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HalfLabel.Location = new System.Drawing.Point(0, 0);
+            this.HalfLabel.Margin = new System.Windows.Forms.Padding(0);
             this.HalfLabel.Name = "HalfLabel";
-            this.HalfLabel.Size = new System.Drawing.Size(83, 22);
+            this.HalfLabel.Size = new System.Drawing.Size(104, 42);
             this.HalfLabel.TabIndex = 3;
             this.HalfLabel.Text = "1st";
             this.HalfLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // splitter3
-            // 
-            this.splitter3.Location = new System.Drawing.Point(186, 0);
-            this.splitter3.Name = "splitter3";
-            this.splitter3.Size = new System.Drawing.Size(90, 28);
-            this.splitter3.TabIndex = 2;
-            this.splitter3.TabStop = false;
-            // 
-            // splitter2
-            // 
-            this.splitter2.Location = new System.Drawing.Point(96, 0);
-            this.splitter2.Name = "splitter2";
-            this.splitter2.Size = new System.Drawing.Size(90, 28);
-            this.splitter2.TabIndex = 1;
-            this.splitter2.TabStop = false;
-            // 
-            // splitter1
-            // 
-            this.splitter1.Location = new System.Drawing.Point(0, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(96, 28);
-            this.splitter1.TabIndex = 0;
-            this.splitter1.TabStop = false;
             // 
             // ExpulsionDataView
             // 
@@ -236,7 +182,7 @@ namespace Marcador
             this.ExpulsionDataView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.ExpulsionDataView.Enabled = false;
             this.ExpulsionDataView.EnableHeadersVisualStyles = false;
-            this.ExpulsionDataView.Location = new System.Drawing.Point(706, 0);
+            this.ExpulsionDataView.Location = new System.Drawing.Point(691, 15);
             this.ExpulsionDataView.MultiSelect = false;
             this.ExpulsionDataView.Name = "ExpulsionDataView";
             this.ExpulsionDataView.ReadOnly = true;
@@ -244,7 +190,7 @@ namespace Marcador
             this.ExpulsionDataView.RowHeadersVisible = false;
             this.ExpulsionDataView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.ExpulsionDataView.ShowEditingIcon = false;
-            this.ExpulsionDataView.Size = new System.Drawing.Size(288, 362);
+            this.ExpulsionDataView.Size = new System.Drawing.Size(288, 332);
             this.ExpulsionDataView.TabIndex = 1;
             this.ExpulsionDataView.Visible = false;
             this.ExpulsionDataView.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.ExpulsionDataView_RowsAdded);
@@ -300,11 +246,11 @@ namespace Marcador
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Marcador";
+            this.Padding = new System.Windows.Forms.Padding(15);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "ScoreBoard";
             this.TransparencyKey = System.Drawing.Color.LimeGreen;
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ScoreBoardPanel.ResumeLayout(false);
             this.ScoreBoardSplitContainer.Panel1.ResumeLayout(false);
             this.ScoreBoardSplitContainer.Panel2.ResumeLayout(false);
@@ -322,15 +268,9 @@ namespace Marcador
         public System.Windows.Forms.Label Team2Label;
         public System.Windows.Forms.Label ScoreLabel;
         public System.Windows.Forms.Label Team1Label;
-        private System.Windows.Forms.Splitter Team2Split;
-        private System.Windows.Forms.Splitter ScoreSplit;
-        private System.Windows.Forms.Splitter Team1Split;
         public System.Windows.Forms.Label TimeOutLabel;
         public System.Windows.Forms.Label CronoLabel;
         public System.Windows.Forms.Label HalfLabel;
-        private System.Windows.Forms.Splitter splitter3;
-        private System.Windows.Forms.Splitter splitter2;
-        private System.Windows.Forms.Splitter splitter1;
         public System.Windows.Forms.DataGridView ExpulsionDataView;
         private System.Windows.Forms.DataGridViewTextBoxColumn Number;
         private System.Windows.Forms.DataGridViewTextBoxColumn Time;
